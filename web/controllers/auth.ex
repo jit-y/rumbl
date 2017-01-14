63e8849f2efe48b59c7aa5d21b873cc321de1a23
yuji takahashi <jit.y.31@gmail.com>
2017-01-15 01:16:21 +0900

    Add videos

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,6 +1,10 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+  import Phoenix.Controller
+  alias Rumbl.Router.Helpers
+
   def init(opts) do
     Keyword.fetch!(opts, :repo)
   end
@@ -35,6 +39,17 @@ defmodule Rumbl.Auth do
       true ->
         dummy_checkpw
         {:error, :not_found, conn}
+    end
+  end
+
+  def authenticate_user(conn, _opt) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt
     end
   end
 end
