c0e78a2e9f39f55056f7b1bcbac92bf346ebe6da
yuji takahashi <jit.y.31@gmail.com>
2017-01-28 18:04:54 +0900

    Add timeout to info_sys

--- lib/rumbl/info_sys.ex
+++ lib/rumbl/info_sys.ex
@@ -28,11 +28,15 @@ defmodule Rumbl.InfoSys do
     {pid, monitor_ref, query_ref}
   end
 
-  defp await_results(children, _opts) do
-    await_result(children, [], :infinity)
+  defp await_results(children, opts) do
+    timeout = opts[:timeout] || 5000
+    timer = Process.send_after(self, :timedout, timeout)
+    results = await_result(children, [], :infinity)
+    cleanup(timer)
+    results
   end
 
-  def await_result([head|tail], acc, timeout) do
+  defp await_result([head|tail], acc, timeout) do
     {pid, monitor_ref, query_ref} = head
     receive do
       {:results, ^query_ref, results} ->
@@ -40,9 +44,31 @@ defmodule Rumbl.InfoSys do
         await_result(tail, results ++ acc, timeout)
       {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
         await_result(tail, acc, timeout)
+      :timedout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
+    after
+      timeout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
     end
   end
-  def await_result([], acc, _) do
+
+  defp await_result([], acc, _) do
     acc
+  end
+
+  defp kill(pid, ref) do
+    Process.demonitor(ref, [:flush])
+    Process.exit(pid, :kill)
+  end
+
+  defp cleanup(timer) do
+    :erlang.cancel_timer(timer)
+    receive do
+      :timedout -> :ok
+    after
+      0 -> :ok
+    end
   end
 end
